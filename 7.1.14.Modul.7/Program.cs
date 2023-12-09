4cbfff13dd74a61e7e2da15e4a795062bc41b095
tyr1kqq <igorturkinwhite@yandex.ru>
2023-12-09 21:26:28 +0200

    Добавьте файлы проекта.

---
+++ 7.1.14.Modul.7/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace _7._1._14.Modul._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+     
+    }
+    class Employee
+    {
+        public string Name;
+        public int Age;
+        public int Salary;
+    }
+    class ProgectManeger : Employee
+    {
+        public string ProjectName;
+    }
+    class Developer : Employee
+    {
+        private string ProgrammingLanguage;
+    }
+
+    class Food { }
+
+    class Fruit : Food { }
+    class Vegetable : Food { } 
+    class Aple : Fruit { }
+    class Banana : Fruit { }
+    class Pear : Fruit { }
+    class Potato : Vegetable { }
+    class  Carrot : Vegetable { }
+
+    class Obj
+    {
+        private string name;
+        private string owner;
+        private int length;
+        private int count;
+
+        public Obj(string name, string ownerName, int objLength, int count)
+        {
+            this.name = name;
+            owner = ownerName;
+            length = objLength;
+            this.count = count;
+        }
+    }
+    class BaseClass
+    {
+        protected string Name;
+
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class DerivedClass : BaseClass 
+    {
+        public string Description;
+
+        public int Counter;
+        public DerivedClass(string name,string description) : base(name)
+        {
+            Description = description;
+        }
+        public DerivedClass(string description, int counter , string name) : base(name) 
+        {
+            Description = description;
+            Counter = counter;
+        }
+    }
+
+}
+
+
+
+
+
---
+++ Modul.7.OOP/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Modul._7.OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new HybridCar();
+            car.Move();
+        }
+
+    }
+    class Car
+    {
+        public double Fuel;
+        public int Milinage;
+
+        public Car()
+        {
+            Fuel = 50;
+            Milinage = 0;
+        }
+        public void Move()
+        {
+            Milinage++;
+            Fuel -= 0.5;
+        }
+        public void FillTheCar()
+        {
+            Fuel = 50;
+        }
+
+    }
+    enum TypeCar
+    {
+        Gas = 0,
+        Electricity
+    }
+    class HybridCar : Car
+    {
+        public TypeCar TypeCar;
+
+        public void ChangeTypeCar(TypeCar type)
+        {
+            TypeCar = type;
+        }
+    }
+
+}
