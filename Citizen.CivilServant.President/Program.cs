b9817a417b4ed4d9c049da95c26808a88cefcd55
tyr1kqq <igorturkinwhite@yandex.ru>
2023-12-10 12:11:52 +0200

    Age in arbait

---
+++ Citizen.CivilServant.President/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Citizen.CivilServant.President
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
