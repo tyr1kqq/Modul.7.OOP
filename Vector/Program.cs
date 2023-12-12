a2ba81d0d7a8cc9223e7a2d434bbd3619538a0c8
tyr1kqq <igorturkinwhite@yandex.ru>
2023-12-12 09:31:15 +0200

    Добавленно сложение векторов

---
+++ Vector/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Vector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a = new Vector { x = 2, y = 6 };
+            Vector b = new Vector { x = 3, y = 4 };
+
+            Vector c = a.Add(b);
+
+            Console.WriteLine($"{c.x} , {c.y}");
+        }
+    }
+    class Vector
+    {
+        public int x;
+        public int y;
+
+        public Vector Add(Vector second)
+        {
+            return new Vector
+            {
+                x = this.x + second.x,
+                y = this.y + second.y
+            };
+        }
+    }
+}
